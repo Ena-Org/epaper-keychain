6b400bef81dc766cbfb8c783b5eaca0c735319b1
Ena_Heleneto <by15242952083@outlook.com>
2025-11-10 01:33:51 +0800

    更新 platformio.ini 和 main.cpp，添加 SPI 配置和显示初始化，优化 LED 控制逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,54 @@
+// Core & project headers
 #include <Arduino.h>
 #include "pins.hpp"
 #include <led.hpp>
+// #include <Arduino_GFX_Library.h>
 
-// using namespace Board;
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_GC9A01A.h>
 
-// Led led(pins.led, pins.ledActiveHigh);
+// 固定到 Round Display 的引脚
+// #define TFT_CS   D1
+// #define TFT_DC   D3
+// #define TFT_BL   D6
+// #define TFT_SCK  D8
+// #define TFT_MOSI D10
+// // 没有用到 MISO，传 -1
 
-void setup()
-{
-  // Serial.begin(115200);
-  // delay(300);
+// // ESP32 专用 SPI 总线 + GC9A01 圆屏
+// Arduino_DataBus *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCK, TFT_MOSI, -1);
+// Arduino_GFX *gfx    = new Arduino_GC9A01(bus, -1 /*RST 没接就 -1*/, 0 /*rotation*/, true /*IPS*/);
 
-  // pinMode(pins.led, OUTPUT);
-  // digitalWrite(pins.led, pins.ledActiveHigh ? HIGH : LOW);
 
-  // 如果板载 NeoPixel 可用，则点亮为绿色
-  // #if (NEOPIXEL_PIN >= 0)
-  //   neopixelWrite(NEOPIXEL_PIN, 0, 255, 0);
-  // #endif
-  pinMode(USER_LED_PIN, OUTPUT);
-  digitalWrite(USER_LED_PIN, LOW);
+#define TFT_CS   D1
+#define TFT_DC   D3
+#define TFT_BL   D6
+#define TFT_SCK  D8
+#define TFT_MOSI D10
+#define TFT_RST  -1   // 没接复位脚就 -1
+
+SPIClass spi = SPI;
+Adafruit_GC9A01A tft(&spi, TFT_CS, TFT_DC, TFT_RST);
+
+void setup() {
+  // 背光脚（D6），必须拉高
+  // 背光必须拉高，不然你永远觉得“没点亮”
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+
+  // 指定 SPI 针脚，然后初始化屏
+  spi.begin(TFT_SCK, /*MISO*/ -1, TFT_MOSI);
+  tft.begin();           // 分辨率默认 240x240
+  tft.setRotation(0);
+
+  tft.fillScreen(GC9A01A_BLACK);
+  tft.fillRect(0, 0, 240, 240, GC9A01A_GREEN);
+  tft.setCursor(30, 110);
+  tft.setTextColor(GC9A01A_WHITE);
+  tft.setTextSize(2);
+  tft.print("GC9A01 OK");
 }
 
-void loop()
-{
-  // led.toggle();
-  // delay(500);
+void loop() {
 }
