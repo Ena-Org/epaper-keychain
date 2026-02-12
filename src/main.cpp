19b9ec6fcef5cf4160712c1b93edf30c0c1ceefa
Ena_Heleneto <by15242952083@outlook.com>
2026-02-12 20:33:12 +0800

    删除 WiFi 管理和文本同步相关的实现代码，简化主程序结构

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,7 @@ static Touch touch;
 #define API_TEXT_URL "http://185.106.176.15:3000/v1/epaper/test"
 #endif
 
-#ifndef WIFI_SSID
-#define WIFI_SSID "CMCC-202-5G"
-#endif
 
-#ifndef WIFI_PASSWORD
-#define WIFI_PASSWORD "13942929840000"
-#endif
-
-WifiManager wifi;
-SimpleHttp http;
-TextSync textSync(http);
 
 void setup()
 {
@@ -40,34 +30,10 @@ void setup()
     EPaper::showBlackWhiteTest();
     Serial.println(">>> setup: after showBlackWhiteTest");
 
-    wifi.init(WIFI_SSID, WIFI_PASSWORD);
-
-    http.setTimeout(8000);
-    textSync.begin(API_TEXT_URL);
-    // 服务器要求 POST 调用，默认为空 body 和 JSON Content-Type，如需自定义可修改
-    textSync.setPostMode(true, "", "application/json");
-
-    String initialText;
-    if (wifi.isConnected() && textSync.fetchNow(initialText))
-        RenderEPaper::render(initialText.c_str());
+  
 }
 
 void loop()
 {
-    wifi.loop();
-
-    if (!wifi.isConnected())
-    {
-        delay(50);
-        return;
-    }
-
-    String text;
-    if (textSync.poll(text))
-    {
-        RenderEPaper::render(text.c_str());
-    }
-
-    // led.loop();
     delay(10);
 }
