33972fc66df0ef95c4151ba006a698858d3299ac
Ena_Helento <by15242952083@outlook.com>
2026-02-25 23:48:16 +0800

    feat: 添加 Transport 类及其相关功能，重构主程序以移除 cmd_router 依赖，新增 codec 和 router 模块

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,14 @@
 #include "render_epaper/render_epaper.hpp"
 
 #include "uploader/uploader.hpp"
-#include "cmd_router/cmd_router.hpp"
+// #include "cmd_router/cmd_router.hpp"
 
 static Led led;
 static Touch touch;
 static Wifi wifi;
 
 static Uploader::UploadSession uploadSession;
-static CmdRouter cmdRouter;
+// static CmdRouter cmdRouter;
 static constexpr uint32_t WIFI_CONNECT_TIMEOUT_MS = 15000;
 
 void setup()
